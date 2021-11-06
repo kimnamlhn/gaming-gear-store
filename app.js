a35e1e106581131f9bbc10fef0885b94cd1310de
harryleitc <harry.l@itcgroup.io>
2021-11-06 22:22:31 +0700

    hbs layout

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs  = require('express-handlebars');
+var express_handlebars_sections = require('express-handlebars-sections');
 const app = express()
 const port = 3000
 
@@ -7,8 +8,17 @@ const port = 3000
 //set static forder public
 app.use(express.static(__dirname + '/public'));
 
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
+app.engine('hbs', exphbs({
+  defaultLayout: 'main.hbs',
+  extname: '.hbs',
+  layoutsDir: 'views/_layouts',
+  partialsDir: 'views/_partials',
+  helpers: {
+    section: express_handlebars_sections(),
+  },
+}
+));
+app.set('view engine', 'hbs');
 
 app.get('/', function (req, res) {
     res.render('home');
