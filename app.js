962f8d9c3d6a885c3c9486ecaf07652f7af1e3b0
Huỳnh Gia Toại <giatoai159@gmail.com>
2021-11-17 14:28:19 +0700

    express-generator

--- app.js
+++ app.js
@@ -1,33 +1,41 @@
-const express = require('express')
-const exphbs  = require('express-handlebars');
-var express_handlebars_sections = require('express-handlebars-sections');
-const app = express()
-const port = 3000
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-//set static forder public
-app.use(express.static(__dirname + '/public'));
+var app = express();
 
-app.engine('hbs', exphbs({
-  defaultLayout: 'main.hbs',
-  extname: '.hbs',
-  layoutsDir: 'views/_layouts',
-  partialsDir: 'views/_partials',
-  helpers: {
-    section: express_handlebars_sections(),
-  },
-}
-));
+// Views
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+// Middlewares
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+// Routes
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-app.get('/', function (req, res) {
-    res.render('home');
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-require('./middlewares/error.mdw')(app);
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
-
-app.listen(port, () => {
-  console.log(`Web app listening at http://localhost:${port}`)
-})
+module.exports = app;
