461c042cb0daa5f62f4df3f33c357245af93beb7
Huỳnh Gia Toại <giatoai159@gmail.com>
2021-11-21 21:48:25 +0700

    modified:   app.js 	modified:   models/index.js 	modified:   package-lock.json 	modified:   package.json 	modified:   views/partials/user/header.hbs

--- models/index.js
+++ models/index.js
@@ -1,5 +1,14 @@
 const {Sequelize} = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_URL);
+// const sequelize = new Sequelize(process.env.DB_URL);
+const sequelize = new Sequelize('mydb', process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: 'localhost',
+    dialect: 'mysql'
+});
 
-module.exports = models;
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+module.exports = db;
