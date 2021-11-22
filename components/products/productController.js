3897e7068cae07798ae1d72e642a2eb7168498c2
Huỳnh Gia Toại <giatoai159@gmail.com>
2021-11-23 00:02:14 +0700

    fix category product list

--- components/products/productController.js
+++ components/products/productController.js
@@ -2,6 +2,7 @@ const productService = require('./productService');
 
 const list = async (req,res) => {
     const products = await productService.list();
+    console.log(JSON.stringify(products));
     res.render('user/productList', { 
         title: 'Electro - Product List',
         products, 
--- components/products/productService.js
+++ components/products/productService.js
@@ -3,6 +3,10 @@ const {models} = require('../../models');
 const list = () => {
     return models.product.findAll({
         raw : true,
+        attributes: ['idProduct', 'name', 'price','images'],
+        include : [
+            {model:models.category, attributes: ['nameCategory'], as: 'category_category'},
+        ],
     });
 };
 module.exports = {list};
