f08d65f6b9995f565b09b2c697e9fc2c0203873f
Huỳnh Gia Toại <giatoai159@gmail.com>
2021-11-24 16:50:55 +0700

    stable commit

--- components/accounts/accountController.js
+++ components/accounts/accountController.js
@@ -1,18 +1,18 @@
 const login = async (req,res) => {
     try {
         res.render('login', {layout:false});
-    } catch (err) {
-        res.render('error',{error});
+    } catch (error) {
+        
     }
 }
 const register = async (req,res) => {
     try {
         res.send('asd');
-    } catch (err) {
+    } catch (error) {
         res.render('error',{error});
     }
 }
-module.exports ={
+module.exports = {
     login,
     register,
 }
--- components/accounts/accountRouter.js
+++ components/accounts/accountRouter.js
@@ -1,15 +1,19 @@
 const express = require('express');
-const passport = require('../../auth/passport');
+// const passport = require('../../auth/passport');
 const router = express.Router();
 
 const accountController = require('./accountController');
 
 /* GET home page. */
-router.post('/login', accountController.login
-    // passport.authenticate('local', {
-    //     successRedirect: '/',
-    //     failureRedirect: '/login' 
-);
+router.get('/login', accountController.login);
+
+// router.post('/login',
+//     passport.authenticate('local'),
+//     function(req, res) {
+//         if(req.user) res.redirect('/login');
+//         else res.redirect('/login');
+//     }
+// );
 
 router.get('/register', accountController.register);
 
