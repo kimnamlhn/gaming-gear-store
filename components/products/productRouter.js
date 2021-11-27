1a4847b6df558c00fc856dc2939ba1c9057436cf
Huỳnh Gia Toại <giatoai159@gmail.com>
2021-11-27 13:53:45 +0700

    review POST

--- components/products/productRouter.js
+++ components/products/productRouter.js
@@ -6,5 +6,12 @@ const productController = require('./productController');
 router.get('/', productController.list); // Product list
 router.get('/checkout', productController.checkout); // Product checkout
 router.get('/:productID', productController.details); // Product details
+router.post('/:productID', function (req, res) {
+    // console.log('name: ',req.body.review_name);
+    // console.log('review:', req.body.review_content);
+    // console.log('rating:', req.body.review_rating);
+    const id = req.params.productID;
+    res.redirect(`${id}`);
+})
 
 module.exports = router;
