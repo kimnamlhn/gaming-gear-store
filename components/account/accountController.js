23fd13fa40d6e4c687bd51871d1dc45c40a190d8
Huỳnh Gia Toại <giatoai159@gmail.com>
2021-11-29 10:13:12 +0700

    fixed delete product

--- components/account/accountController.js
+++ components/account/accountController.js
@@ -1,106 +1,120 @@
-const productService = require('../products/productService')
+const productService = require('../products/productService');
 
-const login = async (req,res) => {
-    try {
-        res.render('account/login', {layout:'auth',title:'Login'});
-    } catch (error) {
-        res.render('error',{error});
-    }
-}
+const login = async (req, res) => {
+	try {
+		res.render('account/login', { layout: 'auth', title: 'Login' });
+	} catch (error) {
+		res.render('error', { error });
+	}
+};
 
-const register = async (req,res) => {
-    try {
-        res.render('account/register',{layout:'auth', title:'Register'});
-    } catch (error) {
-        res.render('error',{error});
-    }
-}
+const register = async (req, res) => {
+	try {
+		res.render('account/register', { layout: 'auth', title: 'Register' });
+	} catch (error) {
+		res.render('error', { error });
+	}
+};
 
-const forgotPassword = async (req,res) => {
-    try {
-        res.render('account/forgot-password',{layout:'auth', title:'Forgot Password'});
-    } catch (error) {
-        res.render('error',{error});
-    }
-}
+const forgotPassword = async (req, res) => {
+	try {
+		res.render('account/forgot-password', {
+			layout: 'auth',
+			title: 'Forgot Password',
+		});
+	} catch (error) {
+		res.render('error', { error });
+	}
+};
 
-const userIndex = async (req,res) => {
-    try {
-        res.render('account/user/index', {layout:'user/account',title:'Main'});
-    } catch (error) {
-        res.render('error',{error});
-    }
-}
+const userIndex = async (req, res) => {
+	try {
+		res.render('account/user/index', {
+			layout: 'user/account',
+			title: 'Main',
+		});
+	} catch (error) {
+		res.render('error', { error });
+	}
+};
 
-const adminIndex = async (req,res) => {
-    try {
-        res.render('account/admin/index', {layout:'admin/account',title:'Main'});
-    } catch (error) {
-        res.render('error',{error});
-    }
-}
+const adminIndex = async (req, res) => {
+	try {
+		res.render('account/admin/index', {
+			layout: 'admin/account',
+			title: 'Main',
+		});
+	} catch (error) {
+		res.render('error', { error });
+	}
+};
 
-const list = async (req,res) => {
-    try {
-        const products = await productService.getAllProductsAdmin();
-        res.render('account/admin/productList', {layout:'admin/account',title:'Product List',products});
-    } catch (error) {
-        res.render('error',{error});
-    }
-}
+const list = async (req, res) => {
+	try {
+		const products = await productService.getAllProductsAdmin();
+		res.render('account/admin/productList', {
+			layout: 'admin/account',
+			title: 'Product List',
+			products,
+		});
+	} catch (error) {
+		res.render('error', { error });
+	}
+};
 
 const addProduct = async (req, res) => {
-    try {
-        res.render('account/admin/addProduct', {layout:'admin/account',title: 'Add a product'})
-    } catch (error) {
-        res.render('error',{error})
-    }
-}
+	try {
+		res.render('account/admin/addProduct', {
+			layout: 'admin/account',
+			title: 'Add a product',
+		});
+	} catch (error) {
+		res.render('error', { error });
+	}
+};
 
 const deleteProduct = async (req, res) => {
-    try {
-        const idProduct = req.body.idProduct;  
-        console.log("id body",idProduct);
-        await productService.deleteProduct(idProduct);   
-        res.redirect(req.headers.referer);
-    } catch (error) {
-        res.render('error',{error})
-    }
-}
+	try {
+		const idProduct = req.body.idProduct;
+		console.log('id body', idProduct);
+		await productService.deleteProduct(idProduct);
+		res.redirect(req.headers.referer);
+	} catch (error) {
+		res.render('error', { error });
+	}
+};
 const addProductPost = async (req, res) => {
-    try {
-        if(req.body.product_name != null && req.body.brand != null){ //can check lai dieu kien validation
-            const entity = {
-                idProduct: null,
-                name : req.body.product_name,
-                category : req.body.product_category,
-                brand : req.body.brand,
-                stock : req.body.product_stock,
-                price : req.body.product_price,
-                thumbnail : req.body.product_thumbnail,
-                images : req.body.product_images,
-                generalInfo : req.body.product_generalinfo,
-                desciption : req.body.product_desciption,
-    
-            }
-
-            await productService.createProduct(entity);
-        }
-
-        res.redirect(req.headers.referer);
-    } catch (error) {
-        res.render('error',{error})
-    }
-}
+	try {
+		if (req.body.product_name != null && req.body.product_brand != null) {
+			//can check lai dieu kien validation
+			const entity = {
+				idProduct: null,
+				name: req.body.product_name,
+				category: req.body.product_category,
+				brand: req.body.product_brand,
+				stock: req.body.product_stock,
+				price: req.body.product_price,
+				thumbnail: req.body.product_thumbnail,
+				images: null,
+				generalInfo: req.body.product_generalinfo,
+				desciption: req.body.product_desciption,
+			};
+			await productService.createProduct(entity);
+		}
+		res.redirect(req.headers.referer);
+	} catch (error) {
+		res.render('error', { error });
+	}
+};
 
 module.exports = {
-    login,
-    register,
-    forgotPassword,
-    userIndex,
-    adminIndex,
-    list,
-    addProduct,
-    deleteProduct,
-    addProductPost
-}
+	login,
+	register,
+	forgotPassword,
+	userIndex,
+	adminIndex,
+	list,
+	addProduct,
+	deleteProduct,
+	addProductPost,
+};
--- components/products/productService.js
+++ components/products/productService.js
@@ -1,226 +1,272 @@
-const {models} = require('../../models');
+const { models } = require('../../models');
 const sequelize = require('sequelize');
 const product = require('../../models/product');
 
 // Product List Page
 const pageValidation = (page) => {
-    page = isNaN(page) ? 0 : Number(page);
-    page = page >= 0 ? page : 0;
-    return page;
-}
+	page = isNaN(page) ? 0 : Number(page);
+	page = page >= 0 ? page : 0;
+	return page;
+};
 
 const getProductCount = () => {
-    return models.product.count();
-}
+	return models.product.count();
+};
 
-const getAllProducts = (categoryId, page = 0,itemsPerPage = 9) => {
-    let where = {};
-    if (!isNaN(categoryId)) {
-        where = {category:categoryId};
-    }
-    return models.product.findAll({
-        raw: true,
-        where,
-        attributes: ['idProduct', 'name', 'brand', 'price','thumbnail'],
-        include: [
-            {model: models.category, attributes: ['nameCategory'], as: 'category_category'},
-            {model: models.product_comments, attributes: [[sequelize.fn('AVG',sequelize.col('rating')),'AvgRating']], as: 'product_comments'}
-        ],
-        distinct: true,
-        group: ['idProduct'],
-        limit: itemsPerPage,
-        offset: page * itemsPerPage,
-        subQuery: false,
-    })
+const getAllProducts = (categoryId, page = 0, itemsPerPage = 9) => {
+	let where = {};
+	if (!isNaN(categoryId)) {
+		where = { category: categoryId };
+	}
+	return models.product.findAll({
+		raw: true,
+		where,
+		attributes: ['idProduct', 'name', 'brand', 'price', 'thumbnail'],
+		include: [
+			{
+				model: models.category,
+				attributes: ['nameCategory'],
+				as: 'category_category',
+			},
+			{
+				model: models.product_comments,
+				attributes: [
+					[sequelize.fn('AVG', sequelize.col('rating')), 'AvgRating'],
+				],
+				as: 'product_comments',
+			},
+		],
+		distinct: true,
+		group: ['idProduct'],
+		limit: itemsPerPage,
+		offset: page * itemsPerPage,
+		subQuery: false,
+	});
 };
 
 const getProductBrandsCount = () => {
-    return models.product.findAll({
-        attributes: ['brand',[sequelize.fn('COUNT',sequelize.col('brand')),'numProducts']],
-        group: ['brand'],
-        raw: true,
-    })
-}
+	return models.product.findAll({
+		attributes: [
+			'brand',
+			[sequelize.fn('COUNT', sequelize.col('brand')), 'numProducts'],
+		],
+		group: ['brand'],
+		raw: true,
+	});
+};
 
 const getProductCategoriesCount = () => {
-    return models.product.findAll({
-        include : [{
-            model:models.category, attributes: ['nameCategory'], as: 'category_category',
-            },
-        ],
-        attributes: ['category',[sequelize.fn('COUNT',sequelize.col('category')),'numProducts']],
-        group: ['category'],
-        raw: true,
-    })
-}
+	return models.product.findAll({
+		include: [
+			{
+				model: models.category,
+				attributes: ['nameCategory'],
+				as: 'category_category',
+			},
+		],
+		attributes: [
+			'category',
+			[sequelize.fn('COUNT', sequelize.col('category')), 'numProducts'],
+		],
+		group: ['category'],
+		raw: true,
+	});
+};
 
 const getProductSortByRating = () => {
-    return models.product.findAll({
-        raw: true,
-        attributes: ['idProduct', 'name', 'brand', 'price','thumbnail',],
-        include: [
-            {model: models.category, attributes: ['nameCategory'], as: 'category_category'},
-            {model: models.product_comments, attributes: [[sequelize.fn('AVG',sequelize.col('rating')),'AvgRating']], as: 'product_comments'}
-        ],
-        order: [
-            [sequelize.literal("`product_comments.AvgRating`"),'DESC'],
-        ],
-        group: ['idProduct'],
-    })
-}
+	return models.product.findAll({
+		raw: true,
+		attributes: ['idProduct', 'name', 'brand', 'price', 'thumbnail'],
+		include: [
+			{
+				model: models.category,
+				attributes: ['nameCategory'],
+				as: 'category_category',
+			},
+			{
+				model: models.product_comments,
+				attributes: [
+					[sequelize.fn('AVG', sequelize.col('rating')), 'AvgRating'],
+				],
+				as: 'product_comments',
+			},
+		],
+		order: [[sequelize.literal('`product_comments.AvgRating`'), 'DESC']],
+		group: ['idProduct'],
+	});
+};
 // Product Details Page
 const getDetails = (id) => {
-    return models.product.findOne({
-        where: {
-            idProduct: id,
-        },
-        raw : true,
-        include : [{
-                model:models.category, attributes: ['nameCategory'], as: 'category_category',
-            },
-        ],
-    });
-}
+	return models.product.findOne({
+		where: {
+			idProduct: id,
+		},
+		raw: true,
+		include: [
+			{
+				model: models.category,
+				attributes: ['nameCategory'],
+				as: 'category_category',
+			},
+		],
+	});
+};
 
 const getDetailImages = (id) => {
-    return models.product_images.findAll({
-        where: {
-            product: id,
-        },
-        raw : true,
-    });
-}
+	return models.product_images.findAll({
+		where: {
+			product: id,
+		},
+		raw: true,
+	});
+};
 
 const getDetailComments = (id, page = 0) => {
-    return models.product_comments.findAndCountAll({
-        offset: page * 3,
-        limit: 3,
-        attributes: ['name','content','rating',[sequelize.fn('date_format', sequelize.col('creationAt'), '%d %b %Y, %h:%i %p'), 'creationAt']],
-        include: [{
-            model: models.account,
-            attributes: ['name'],
-            as: 'idAccount_account'
-        }],
-        where: {
-            idProduct: id,
-        },
-        raw: true,
-    })
-}
+	return models.product_comments.findAndCountAll({
+		offset: page * 3,
+		limit: 3,
+		attributes: [
+			'name',
+			'content',
+			'rating',
+			[
+				sequelize.fn(
+					'date_format',
+					sequelize.col('creationAt'),
+					'%d %b %Y, %h:%i %p'
+				),
+				'creationAt',
+			],
+		],
+		include: [
+			{
+				model: models.account,
+				attributes: ['name'],
+				as: 'idAccount_account',
+			},
+		],
+		where: {
+			idProduct: id,
+		},
+		raw: true,
+	});
+};
 
-const getDetailsCommentsCount = async (id,count) => {
-    let result = await models.product_comments.findAll({
-        where: {
-            idProduct: id,
-        },
-        attributes: ['rating',[sequelize.fn('COUNT',sequelize.col('rating')),'ratingcount']],
-        group: ['rating'],
-        raw: true,
-    });
-    let ratingAvg = 0;
-    for (let e of result) ratingAvg+= e.rating*e.ratingcount;
-    ratingAvg/=count;
-    return {result,ratingAvg};
-}
+const getDetailsCommentsCount = async (id, count) => {
+	let result = await models.product_comments.findAll({
+		where: {
+			idProduct: id,
+		},
+		attributes: [
+			'rating',
+			[sequelize.fn('COUNT', sequelize.col('rating')), 'ratingcount'],
+		],
+		group: ['rating'],
+		raw: true,
+	});
+	let ratingAvg = 0;
+	for (let e of result) ratingAvg += e.rating * e.ratingcount;
+	ratingAvg /= count;
+	return { result, ratingAvg };
+};
 
 const getDetailRelatedProducts = (id, idCategory) => {
-    return models.product.findAll({
-        where: {
-            idProduct: {
-                [sequelize.Op.not]: id
-            },
-            category: idCategory
-        },
-        attributes: ['idProduct','name','price','brand','thumbnail'],
-        raw: true,
-        limit: 4,
-        include : [{
-            model:models.category, attributes: ['nameCategory'], as: 'category_category',
-        },
-    ],
-    })
-}
+	return models.product.findAll({
+		where: {
+			idProduct: {
+				[sequelize.Op.not]: id,
+			},
+			category: idCategory,
+		},
+		attributes: ['idProduct', 'name', 'price', 'brand', 'thumbnail'],
+		raw: true,
+		limit: 4,
+		include: [
+			{
+				model: models.category,
+				attributes: ['nameCategory'],
+				as: 'category_category',
+			},
+		],
+	});
+};
 
 // Admin Product List Page
 const getAllProductsAdmin = () => {
-    return models.product.findAll({
-        raw: true,
-        include: [
-            {model: models.category, attributes: ['nameCategory'], as: 'category_category'},
-        ],
-    })
+	return models.product.findAll({
+		raw: true,
+		include: [
+			{
+				model: models.category,
+				attributes: ['nameCategory'],
+				as: 'category_category',
+			},
+		],
+	});
 };
 
-const deleteProduct = async (idProduct) => {
-    // const product = await models.product.findOne({
-    //     where: {
-    //         idProduct: idProduct,
-    //     },
-    //     raw : true,
-    // });
+const deleteProduct = async (id) => {
+	// const product = await models.product.findOne({
+	//     where: {
+	//         idProduct: idProduct,
+	//     },
+	//     raw : true,
+	// });
 
-    // await product.destroy();
-    try {
-        let product = await models.product.findOne({ idProduct: idProduct});
-    
-        product.destroy().then(function(){
-    
-            res.status(200).json({
-                message: 'User deleted.'
-            })
-    
-        });
-    
-    } catch (e) {
-        res.render('error',{e})
-    }
-
-}
+	// await product.destroy();
+	try {
+		let product = await models.product.findOne({
+			where: { idProduct: id },
+		});
+		product.destroy().then(function () {
+			// res.status(200).json({
+			// 	message: 'User deleted.',
+			// });
+			console.log('Deleted');
+		});
+	} catch (e) {
+		res.render('error', { e });
+	}
+};
 
 const createProduct = async (entity) => {
-    try {    
+	try {
+		console.log('abc:', entity);
 
-        console.log("abc:", entity);
+		const product = models.product.build({
+			idProduct: null,
+			name: entity.name,
+			category: entity.category,
+			brand: entity.brand,
+			stock: entity.stock,
+			price: entity.price,
+			thumbnail: entity.thumbnail,
+			images: entity.images,
+			generalInfo: entity.generalInfo,
+			desciption: entity.desciption,
+			creationDate: '2021/1/1', //test truoc da
+		});
 
-        const product = models.product.build({  
-            idProduct: null,
-            name : entity.name,
-            category : entity.category,
-            brand : entity.brand,
-            stock : entity.stock,
-            price : entity.price,
-            thumbnail : entity.thumbnail,
-            images : entity.images,
-            generalInfo : entity.generalInfo,
-            desciption : entity.desciption, 
-            creationDate: "1/1/2021" //test truoc da
-        });
-
-        console.log("abc",product);
-        await product.save();
-    
-    } catch (e) {
-        console.log("err:",e);
-    }
-
-}
-
-
-
+		console.log('abc', product);
+		await product.save();
+	} catch (e) {
+		console.log('err:', e);
+	}
+};
 
 module.exports = {
-    pageValidation,
-    getProductCount,
-    getAllProducts,
-    getProductBrandsCount,
-    getProductCategoriesCount,
-    getProductSortByRating,
-    getDetails,
-    getDetailImages,
-    getDetailComments,
-    getDetailsCommentsCount,
-    getDetailRelatedProducts,
-    getAllProductsAdmin,
-    deleteProduct,
-    createProduct
+	pageValidation,
+	getProductCount,
+	getAllProducts,
+	getProductBrandsCount,
+	getProductCategoriesCount,
+	getProductSortByRating,
+	getDetails,
+	getDetailImages,
+	getDetailComments,
+	getDetailsCommentsCount,
+	getDetailRelatedProducts,
+	getAllProductsAdmin,
+	deleteProduct,
+	createProduct,
 };
