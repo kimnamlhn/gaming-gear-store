87f8d3d666d79b25608eea17a7bd35ddd8eeb25e
Huỳnh Gia Toại <giatoai159@gmail.com>
2021-12-16 14:55:02 +0700

    fix error

--- views/helpers/index.js
+++ views/helpers/index.js
@@ -1,11 +1,9 @@
 var express_handlebars_sections = require('express-handlebars-sections');
 
 module.exports = {
-    addNum: require('./addNum'),
-    checkIfEqual: require('./checkIfEqual'),
-    pagination: require('./pagination'),
-    lowerCase: require('./lowerCase'),
-    section: express_handlebars_sections(),  
-    for: require('./forloop'),
-    rating_star: require('./rating_star'),
+	addNum: require('./addNum'),
+	checkIfEqual: require('./checkIfEqual'),
+	lowerCase: require('./lowerCase'),
+	section: express_handlebars_sections(),
+	for: require('./forloop'),
 };
