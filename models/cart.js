d6693ca73bcc645571299fc7dac4d64167b27768
Huỳnh Gia Toại <giatoai159@gmail.com>
2021-12-22 00:53:00 +0700

    fix

--- models/cart.js
+++ models/cart.js
@@ -1,51 +1,53 @@
 const Sequelize = require('sequelize');
-module.exports = function (sequelize, DataTypes) {
-	return sequelize.define(
-		'cart',
-		{
-			idCart: {
-				autoIncrement: true,
-				type: DataTypes.INTEGER,
-				allowNull: false,
-				primaryKey: true,
-			},
-			idAccount: {
-				type: DataTypes.INTEGER,
-				allowNull: true,
-				references: {
-					model: 'account',
-					key: 'idAccount',
-				},
-				unique: 'fk_account_cart',
-			},
-			idSession: {
-				type: DataTypes.STRING(45),
-				allowNull: true,
-			},
-		},
-		{
-			sequelize,
-			tableName: 'cart',
-			timestamps: true,
-			indexes: [
-				{
-					name: 'PRIMARY',
-					unique: true,
-					using: 'BTREE',
-					fields: [{ name: 'idCart' }],
-				},
-				{
-					name: 'idAccount_UNIQUE',
-					unique: true,
-					using: 'BTREE',
-					fields: [{ name: 'idAccount' }],
-				},
-				{
-					name: 'fk_account_cart_idx',
-					using: 'BTREE',
-					fields: [{ name: 'idAccount' }],
-				},
-			],
-		}
-	);
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('cart', {
+    idCart: {
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
+    idAccount: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: 'account',
+        key: 'idAccount'
+      },
+      unique: "fk_account_cart"
+    },
+    idSession: {
+      type: DataTypes.STRING(45),
+      allowNull: true
+    }
+  }, {
+    sequelize,
+    tableName: 'cart',
+    timestamps: true,
+    indexes: [
+      {
+        name: "PRIMARY",
+        unique: true,
+        using: "BTREE",
+        fields: [
+          { name: "idCart" },
+        ]
+      },
+      {
+        name: "idAccount_UNIQUE",
+        unique: true,
+        using: "BTREE",
+        fields: [
+          { name: "idAccount" },
+        ]
+      },
+      {
+        name: "fk_account_cart_idx",
+        using: "BTREE",
+        fields: [
+          { name: "idAccount" },
+        ]
+      },
+    ]
+  });
 };
